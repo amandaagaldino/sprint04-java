a8b035d32cda0c6c06ddb8d779dd262d84b8f42d
amandaagaldino <amandagaldinox@gmail.com>
2025-05-08 10:51:50 -0300

    Criando o repository de estacao e linha, com os metodos getAll e getByIds

---
+++ src/main/java/fiap/tds/repositories/EstacaoRepository.java
@@ -0,0 +1,68 @@
+package fiap.tds.repositories;
+
+import fiap.tds.entities.enums.TIPOS_ALERTA;
+import fiap.tds.entities.objects.Alerta;
+import fiap.tds.entities.objects.Estacao;
+import fiap.tds.infrastructure.DatabaseConfig;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class EstacaoRepository {
+    List<Estacao> estacaos = new ArrayList<>();
+
+    public List<Estacao> getAll() {
+        var query = "SELECT * FROM \"T_TT_ESTACAO\"";
+        try (var connection = DatabaseConfig.getConnection()) {
+            var stmt = connection.prepareStatement(query);
+            var result = stmt.executeQuery();
+            while (result.next()){
+                var estacao = new Estacao();
+                estacao.setId(result.getInt("id"));
+                estacao.setNome(result.getString("nome"));
+                estacao.setId_linha(result.getInt("id_linha"));
+
+
+                estacaos.add(estacao);
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao buscar os alertas no banco de dados");
+            e.printStackTrace();
+        }
+        return estacaos;
+    }
+
+
+    public Optional<Estacao> getById(int id) {
+        var query = "SELECT * from \"T_TT_ESTACAO\" where id = ?";
+        try (var connection = DatabaseConfig.getConnection();
+             var preparedStatement = connection.prepareStatement(query)) {
+
+            // Definir o parâmetro na query
+            preparedStatement.setInt(1, id);
+
+            // Executar a consulta ao banco
+            var result = preparedStatement.executeQuery();
+
+            // Se encontrar um resultado, cria o objeto Alerta e retorna
+            if (result.next()) {
+                Estacao estacao = new Estacao();
+                estacao.setId(result.getInt("id"));
+                estacao.setNome(result.getString("nome"));
+                estacao.setId_linha(result.getInt("id_linha"));
+
+               estacaos.add(estacao);
+
+            }
+
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // Caso nenhum registro seja encontrado, retorna Optional.empty()
+        return Optional.empty();
+
+    }
+}
---
+++ src/main/java/fiap/tds/repositories/LinhaRepository.java
@@ -0,0 +1,73 @@
+package fiap.tds.repositories;
+
+import fiap.tds.entities.objects.Alerta;
+import fiap.tds.entities.objects.Estacao;
+import fiap.tds.entities.objects.Linha;
+import fiap.tds.infrastructure.DatabaseConfig;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class LinhaRepository {
+    List<Linha> linhas = new ArrayList<>();
+
+
+    public List<Linha> getAll() {
+        var query = "SELECT * FROM \"T_TT_LINHA\"";
+        try (var connection = DatabaseConfig.getConnection()) {
+            var stmt = connection.prepareStatement(query);
+            var result = stmt.executeQuery();
+            while (result.next()){
+                var linha = new Linha();
+                linha.setId(result.getInt("id"));
+                linha.setNome_linha(result.getString("nome_linha"));
+                linha.setNumero_linha(result.getInt("numero_linha"));
+                linha.setLocalizacao_inicial(result.getString("localizacao_inicial"));
+                linha.setSituacaa_atual(result.getString("situacao_atual"));
+
+
+                linhas.add(linha);
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao buscar os alertas no banco de dados");
+            e.printStackTrace();
+        }
+        return linhas;
+    }
+
+
+    public Optional<Alerta> getById(int id) {
+        var query = "SELECT * from \"T_TT_LINHA\" where id = ?";
+        try (var connection = DatabaseConfig.getConnection();
+             var preparedStatement = connection.prepareStatement(query)) {
+
+            // Definir o parâmetro na query
+            preparedStatement.setInt(1, id);
+
+            // Executar a consulta ao banco
+            var result = preparedStatement.executeQuery();
+
+            // Se encontrar um resultado, cria o objeto Alerta e retorna
+            if (result.next()) {
+                var linha = new Linha();
+                linha.setId(result.getInt("id"));
+                linha.setNome_linha(result.getString("nome_linha"));
+                linha.setNumero_linha(result.getInt("numero_linha"));
+                linha.setLocalizacao_inicial(result.getString("localizacao_inicial"));
+                linha.setSituacaa_atual(result.getString("situacao_atual"));
+
+                linhas.add(linha);
+
+            }
+
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // Caso nenhum registro seja encontrado, retorna Optional.empty()
+        return Optional.empty();
+
+    }
+}
