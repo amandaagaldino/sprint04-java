2f112257212758d8be7349439bf6bd2c1d808f12
amandaagaldino <amandagaldinox@gmail.com>
2025-05-11 00:59:52 -0300

    verificando manutencao

--- src/main/java/fiap/tds/services/ManutencaoService.java
+++ src/main/java/fiap/tds/services/ManutencaoService.java
@@ -1,4 +1,15 @@
 package fiap.tds.services;
 
+import fiap.tds.entities.objects.Manutencao;
+
 public class ManutencaoService {
+    public boolean validateManutencao(Manutencao manutencao){
+        if(manutencao == null)
+            return false;
+        //ver o que precisa escrever para criar a manutencao no front (o id gera sozinho, a hora deve ser gerado sozinha?)
+        if(manutencao.getLocal().isBlank() || manutencao.getDescricao().isBlank())
+            return false;
+
+        return true;
+    }
 }
