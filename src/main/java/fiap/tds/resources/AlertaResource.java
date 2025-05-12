6c5b8a0867a867a2b58d17eee75557e93c1cb26a
amandaagaldino <amandagaldinox@gmail.com>
2025-05-12 12:00:08 -0300

    arrumando

--- src/main/java/fiap/tds/resources/AlertaResource.java
+++ src/main/java/fiap/tds/resources/AlertaResource.java
@@ -1,13 +1,17 @@
 package fiap.tds.resources;
 
+import fiap.tds.entities.objects.Alerta;
 import fiap.tds.repositories.AlertaRepository;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/alerta")
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class  AlertaResource {
 
     public AlertaRepository alertaRepository = new AlertaRepository();
@@ -20,6 +24,7 @@ public class  AlertaResource {
     public Response getAlertas() {
         return Response.ok().entity(alertaRepository.getAll()).build();
     }
+
 
 //    public Response rateLimitFallback(){
 //        return Response.status(Response.Status.TOO_MANY_REQUESTS)
